1c3da1a0dfa3e35a71a3574a67a6ee8bd151676f
TaeYeongKwak <abgudv6403@naver.com>
2022-12-18 17:52:54 +0900

    init

---
+++ src/main/java/com/videochat/online/OnlineVideoChatServerApplication.java
@@ -0,0 +1,13 @@
+package com.videochat.online;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class OnlineVideoChatServerApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(OnlineVideoChatServerApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/videochat/online/OnlineVideoChatServerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.videochat.online;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class OnlineVideoChatServerApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
