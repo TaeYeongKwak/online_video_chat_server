657434e22661173f2d87a6e123a574a8c8e01873
TaeYeongKwak <abgudv6403@naver.com>
2022-12-20 17:38:00 +0900

    [config] swagger 기본 설정

---
+++ src/main/java/com/videochat/online/global/config/SwaggerConfig.java
@@ -0,0 +1,53 @@
+package com.videochat.online.global.config;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.bind.annotation.RestController;
+import springfox.documentation.builders.PathSelectors;
+import springfox.documentation.builders.RequestHandlerSelectors;
+import springfox.documentation.service.ApiKey;
+import springfox.documentation.service.AuthorizationScope;
+import springfox.documentation.service.SecurityReference;
+import springfox.documentation.service.Tag;
+import springfox.documentation.spi.DocumentationType;
+import springfox.documentation.spi.service.contexts.SecurityContext;
+import springfox.documentation.spring.web.plugins.Docket;
+import springfox.documentation.swagger2.annotations.EnableSwagger2;
+
+import java.util.Arrays;
+import java.util.List;
+
+@Configuration
+@EnableSwagger2
+public class SwaggerConfig {
+
+    @Bean
+    public Docket swagger() {
+        return new Docket(DocumentationType.SWAGGER_2)
+                .securityContexts(Arrays.asList(this.securityContext()))
+                .securitySchemes(Arrays.asList(this.apiKey()))
+                .select()
+                .apis(RequestHandlerSelectors.withClassAnnotation(RestController.class))
+                .paths(PathSelectors.any())
+                .build()
+                .tags(
+                        new Tag("1. User", "회원 API")
+                );
+    }
+
+    private SecurityContext securityContext() {
+        return SecurityContext.builder().securityReferences(defaultAuth()).build();
+    }
+
+    private List<SecurityReference> defaultAuth() {
+        AuthorizationScope authorizationScope = new AuthorizationScope("global", "accessEverything");
+        AuthorizationScope[] authorizationScopes = new AuthorizationScope[1];
+        authorizationScopes[0] = authorizationScope;
+        return Arrays.asList(new SecurityReference("Authorization", authorizationScopes));
+    }
+
+    private ApiKey apiKey() {
+        return new ApiKey("Authorization", "Authorization", "header");
+    }
+
+}
--- src/main/java/com/videochat/online/user/controller/UserController.java
+++ src/main/java/com/videochat/online/user/controller/UserController.java
@@ -2,12 +2,15 @@ package com.videochat.online.user.controller;
 
 import com.videochat.online.global.dto.APIResponseDTO;
 import com.videochat.online.user.service.UserService;
+import io.swagger.annotations.Api;
+import io.swagger.annotations.ApiOperation;
 import lombok.RequiredArgsConstructor;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+@Api(tags = "1. User")
 @RestController
 @RequiredArgsConstructor
 @RequestMapping(value = "/user")
@@ -15,6 +18,7 @@ public class UserController {
 
     private final UserService userService;
 
+    @ApiOperation(value = "사용자 정보 상세 조회")
     @GetMapping(value = "/{user-id}")
     public APIResponseDTO getUserInfo(@PathVariable("user-id") Long userId){
         return APIResponseDTO.success(userService.getUserInfo(userId));
